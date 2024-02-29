c99fdc18cee1437c5f0b35c098b502c7213a1c21
mstharindu <supuntharindu21@gmail.com>
2024-02-29 22:54:25 +0530

    Establishing sample socket connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const app = express();
-const port = 5000;
+const { createServer } = require('http');
+const { Server } = require('socket.io');
 
-app.get('/api', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', (socket) => {
+  console.log('socket connected');
+
+  setInterval(() => {
+    socket.emit('interval', new Date());
+  }, 1000);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+httpServer.listen(5000, () => {
+  console.log('server started');
 });
